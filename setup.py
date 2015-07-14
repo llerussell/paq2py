36f706b244ef80a762316026a03c688abd697fac
Lloyd Russell <llerussell@gmail.com>
2015-07-14 09:18:33 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+setup(name='paq2py',
+      version='0.1',
+      description='Read PAQ files into numpy array',
+      author='Lloyd Russell',
+      py_modules=['paq2py'],
+      )
